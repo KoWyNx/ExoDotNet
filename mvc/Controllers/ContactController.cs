51767de6e67c88bc5a48b85795d2c3c37a4b3996
KoWyNx <fberrani@hotmail.fr>
2025-01-13 16:42:18 +0100

    exercice 2 et 3

--- mvc/Controllers/ContactController.cs
+++ mvc/Controllers/ContactController.cs
@@ -1,21 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
 
 namespace mvc.Controllers;
 
 public class ContactController : Controller
 {
+    private static List<Contact> _contacts = new List<Contact>
+    {
+        new Contact { Id = 1, Name = "Test", Email = "test@test.fr", Phone = "123456789" },
+        new Contact { Id = 2, Name = "Super Test", Email = "supertest@supertest.fr", Phone = "987654321" }
+    };
+
     public IActionResult GetAllContact()
     {
-        return View();
-    }
-    
-    public IActionResult GetOneContact()
-    {
-        return View();
+        ViewBag.Message = "Liste des contacts";
+
+        ViewData["Count"] = _contacts.Count;
+
+        return View(_contacts); 
     }
 
+    public IActionResult GetOneContact(int id)
+    {
+        var contact = _contacts.FirstOrDefault(c => c.Id == id);
+        if (contact == null)
+        {
+            var firstContact = _contacts.FirstOrDefault(c => c.Id == 1);
+            return View(firstContact);
+        }
+
+        ViewData["Title"] = $"Détails du contact {contact.Name}";
+
+        ViewBag.Description = "Informations détaillées pour le contact sélectionné.";
+
+        return View(contact); 
+    }
     public IActionResult AddContact()
     {
-        return View();
+        return View(); 
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult AddContact(Contact contact)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(contact); 
+        }
+
+        contact.Id = _contacts.Any() ? _contacts.Max(c => c.Id) + 1 : 1;
+        _contacts.Add(contact);
+
+        return RedirectToAction(nameof(GetAllContact)); 
     }
 }
--- mvc/obj/Debug/net9.0/mvc.AssemblyInfo.cs
+++ mvc/obj/Debug/net9.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d6ecc553b5f5a73324871ed71ab18f1dbc477c")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
